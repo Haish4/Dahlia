0d7ff4b79ef1031a660d9a7409e2a8c1244fc520
Haish4 <muhamadamir912@gmail.com>
2024-12-03 14:29:10 +0800

    siap token, logOut

--- app/src/main/java/com/example/dahlia/activities/MainActivity.java
+++ app/src/main/java/com/example/dahlia/activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.dahlia.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +12,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.dahlia.R;
 import com.example.dahlia.databinding.ActivityMainBinding;
+import com.example.dahlia.utilities.Constants;
 import com.example.dahlia.utilities.PreferenceManager;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         preferenceManager = new PreferenceManager(getApplicationContext());
         setListener();
+        getToken();
     }
 
 
@@ -36,8 +42,22 @@ public class MainActivity extends AppCompatActivity {
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         });
     }
-    private void loadUserDetails() {
 
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+    private void getToken(){
+        FirebaseMessaging.getInstance().getToken().addOnSuccessListener(this::updateToken);
+    }
+
+    private void updateToken (String token){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = database
+                .collection(Constants.KEY_COLLECTION_USERS)
+                .document(preferenceManager.getString(Constants.KEY_USER_ID));
+        documentReference.update(Constants.KEY_FCM_TOKEN, token)
+                .addOnSuccessListener(unused -> showToast("Token Updated"))
+                .addOnFailureListener(e -> showToast("Unable to update Token!!"));
     }
 
 }
--- app/src/main/java/com/example/dahlia/activities/profileActivity.java
+++ app/src/main/java/com/example/dahlia/activities/profileActivity.java
@@ -1,8 +1,14 @@
 package com.example.dahlia.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Base64;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,16 +17,79 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.dahlia.R;
+import com.example.dahlia.databinding.ActivityProfileBinding;
+import com.example.dahlia.utilities.Constants;
+import com.example.dahlia.utilities.PreferenceManager;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.HashMap;
 
 public class profileActivity extends AppCompatActivity {
+
+    private ActivityProfileBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_profile);
-        TextView yourTextView = findViewById(R.id.profileBio);
-        yourTextView.setMovementMethod(new ScrollingMovementMethod());
-
+        setContentView(binding.getRoot());
+        binding.profileBio.setMovementMethod(new ScrollingMovementMethod());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        loadUserDetails();
+        setListener();
     }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+    private void loadUserDetails(){
+        binding.profileName.setText(preferenceManager.getString(Constants.KEY_NAME));
+        byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        binding.profileImage.setImageBitmap(bitmap);
+    }
+
+
+    private void setListener() {
+        binding.logOutButton.setOnClickListener(v -> signOut());
+        binding.backButton.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        });
+    }
+
+    private void signOut(){
+        showToast("Logging Out...");
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = database
+                .collection(Constants.KEY_COLLECTION_USERS)
+                .document(preferenceManager.getString(Constants.KEY_USER_ID));
+        HashMap<String, Object> updates = new HashMap<>();
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        documentReference.update(updates).addOnSuccessListener(unused -> {
+            preferenceManager.clear();
+            startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+        }).addOnFailureListener(e -> showToast("Can't Log Out"));
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+
+
 }
